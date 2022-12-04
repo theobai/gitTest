c3baae8f9bf6a15538a49affda2a1a58e69bdd99
theobai <66500012+theobai@users.noreply.github.com>
2022-12-04 21:58:17 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello master1");
         System.out.println("hello hotfix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
