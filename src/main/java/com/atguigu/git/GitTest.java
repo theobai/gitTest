048c1217d0a55c3130809428e67ff3577af3f9c5
theobai <theo8388@outlook.com>
2022-12-04 14:38:15 +0800

    master commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hello Git3");
         System.out.println("hello Git4");
         System.out.println("hello Github");
-        System.out.println("hello master");
+        System.out.println("hello master1");
     }
 }
