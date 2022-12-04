c7a988aa2ef69c0551c9f4e620b4a7cc8b1fce4f
theobai <theo8388@outlook.com>
2022-12-04 10:13:08 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git2");
+        System.out.println("hello Git3");
         System.out.println("hello Github");
     }
 }
