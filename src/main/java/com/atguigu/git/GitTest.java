a0176efd9bb6fee97037061edd044eb77be17364
theobai <theo8388@outlook.com>
2022-12-04 21:51:16 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello Github");
         System.out.println("hello master1");
         System.out.println("hello hotfix");
+        System.out.println("push test");
     }
 }
